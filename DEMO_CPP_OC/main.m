4ef27fb76cf2d46de3aaa1521de15388656fda97
Wiki <taoweiji2008@qq.com>
2016-07-06 23:13:36 +0800

    Initial Commit

---
+++ DEMO_CPP_OC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DEMO_CPP_OC
+//
+//  Created by Wiki on 16/7/6.
+//  Copyright © 2016年 thejoyrun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
